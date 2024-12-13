3bc5654a2b26f524ab97af947d2cd5476d7f71a6
taeyang0206 <taengsun0206@gmail.com>
2024-12-14 04:10:17 +0900

    modified:   figlet_module.js

--- figlet_module.js
+++ figlet_module.js
@@ -1,6 +1,6 @@
 var figlet = require("figlet");
 
-figlet("SIBERARIAN HUSKY", function (err, data) {
+figlet("EUNJEE LOVE", function (err, data) {
   if (err) {
     console.log("Something went wrong...");
     console.dir(err);
